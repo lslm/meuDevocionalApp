84c9e729acd765084cae1d94b90aa8ebfe001af2
bia.duque <biaduque7@hotmail.com>
2022-01-05 15:48:47 -0300

    Adicionar marks

--- MeuDevocional/DevocionaisDiarias/DevocionalDiarioViewController.swift
+++ MeuDevocional/DevocionaisDiarias/DevocionalDiarioViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import UserNotifications
 
-class DevocionalDiarioViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class DevocionalDiarioViewController: UIViewController {
     
     let reuseIdentifier = "cell"
     let reuseIdentifier2 = "cell2"
@@ -19,7 +19,7 @@ class DevocionalDiarioViewController: UIViewController, UICollectionViewDataSour
     let capaVida = ["vida1","vida2","vida3","vida4","vida5","vida6"]
     let capaEstudos = ["estudos1","estudos2","estudos3","estudos4","estudos5"]
 
-    //declaracao de variaveis
+    // MARK: Declaracao de variaveis
     //colections
     @IBOutlet weak var cotidiano: UICollectionView!
     
@@ -36,83 +36,19 @@ class DevocionalDiarioViewController: UIViewController, UICollectionViewDataSour
     @IBOutlet weak var tituloVida: UILabel?
     @IBOutlet weak var tituloEstudos: UILabel?
     
-    //configuracao da collectionView
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //retorna a quantidade de itens da colectionView
-        if collectionView == cotidiano{
-            return self.capaCotidiano.count
-        }
-        else if collectionView == vida {
-            return self.capaVida.count
-        }
-        return self.capaEstudos.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        if collectionView == cotidiano{
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
-            editaCelulaDev(cell: cell, array: capaCotidiano, index: indexPath.row,tit: titCotidiano,referencia: refCotidiano)
-            return cell
-        }
-        else if collectionView == vida{
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier2, for: indexPath as IndexPath) as! MyCollectionViewCell
-            editaCelulaDev(cell: cell, array: capaVida, index: indexPath.row,tit: titVida,referencia: refVida)
-            return cell
-        }
-        else{
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier3, for: indexPath as IndexPath) as! MyCollectionViewCell
-            editaCelulaDev(cell: cell, array: capaEstudos, index: indexPath.row,tit: titEstudos,referencia: refEstudos)
-            return cell
-        }
-    }
-    
-    //funcao que faz passar para dentro do estudo devocional (ao clicar na cell)
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        if collectionView == cotidiano {
-                if let vc = storyboard?.instantiateViewController(identifier: "devocional") as? DevocionalDiarioEstudoViewController {
-                    vc.cell = 1
-                    vc.estudo = indexPath.row
-                    navigationController?.pushViewController(vc, animated: true)
-                }
-        }else if collectionView == vida{
-            if let vc = storyboard?.instantiateViewController(identifier: "devocional") as?
-                DevocionalDiarioEstudoViewController {
-                vc.cell = 2
-                vc.estudo = indexPath.row
-                navigationController?.pushViewController(vc, animated: true)
-            }
-        }
-        else{
-            if let vc = storyboard?.instantiateViewController(identifier: "devocional") as?
-                DevocionalDiarioEstudoViewController {
-                vc.cell = 3
-                vc.estudo = indexPath.row
-                navigationController?.pushViewController(vc, animated: true)
-            }
-        }
-    }
-    
+    // MARK: View Did Load 
     override func viewDidLoad() {
         super.viewDidLoad()
         //transformando o titulo
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: verde]
         //chama a solicitacao de notificacao
-        criaNotif()
+        self.criaNotif()
         
     }
     
-    func editaCelulaDev(cell: MyCollectionViewCell,array: [String],index: Int,tit:[String],referencia: [String]){
-        cell.image.image = UIImage(named: array[index])
-        cell.titleCard.text = tit[index]
-        cell.textCard.text = referencia[index]
-        cell.titleCard.textColor = verde3
-        cell.textCard.textColor = verde3
-        cell.layer.cornerRadius = 15
-    }
-    
+
+    // MARK: Notificacao
     func criaNotif(){
         //pedindo permissão
         let center = UNUserNotificationCenter.current()
@@ -147,4 +83,76 @@ class DevocionalDiarioViewController: UIViewController, UICollectionViewDataSour
         }
         
     }
+}
+
+// MARK: Collection View
+extension DevocionalDiarioViewController: UICollectionViewDelegate{
+    //funcao que faz passar para dentro do estudo devocional (ao clicar na cell)
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        if collectionView == cotidiano {
+                if let vc = storyboard?.instantiateViewController(identifier: "devocional") as? DevocionalDiarioEstudoViewController {
+                    vc.cell = 1
+                    vc.estudo = indexPath.row
+                    navigationController?.pushViewController(vc, animated: true)
+                }
+        }else if collectionView == vida{
+            if let vc = storyboard?.instantiateViewController(identifier: "devocional") as?
+                DevocionalDiarioEstudoViewController {
+                vc.cell = 2
+                vc.estudo = indexPath.row
+                navigationController?.pushViewController(vc, animated: true)
+            }
+        }
+        else{
+            if let vc = storyboard?.instantiateViewController(identifier: "devocional") as?
+                DevocionalDiarioEstudoViewController {
+                vc.cell = 3
+                vc.estudo = indexPath.row
+                navigationController?.pushViewController(vc, animated: true)
+            }
+        }
+    }
+}
+
+extension DevocionalDiarioViewController: UICollectionViewDataSource{
+    //configuracao da collectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //retorna a quantidade de itens da colectionView
+        if collectionView == cotidiano{
+            return self.capaCotidiano.count
+        }
+        else if collectionView == vida {
+            return self.capaVida.count
+        }
+        return self.capaEstudos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if collectionView == cotidiano{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+            self.editaCelulaDev(cell: cell, array: capaCotidiano, index: indexPath.row,tit: titCotidiano,referencia: refCotidiano)
+            return cell
+        }
+        else if collectionView == vida{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier2, for: indexPath as IndexPath) as! MyCollectionViewCell
+            self.editaCelulaDev(cell: cell, array: capaVida, index: indexPath.row,tit: titVida,referencia: refVida)
+            return cell
+        }
+        else{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier3, for: indexPath as IndexPath) as! MyCollectionViewCell
+            self.editaCelulaDev(cell: cell, array: capaEstudos, index: indexPath.row,tit: titEstudos,referencia: refEstudos)
+            return cell
+        }
+    }
+    func editaCelulaDev(cell: MyCollectionViewCell,array: [String],index: Int,tit:[String],referencia: [String]){
+        cell.image.image = UIImage(named: array[index])
+        cell.titleCard.text = tit[index]
+        cell.textCard.text = referencia[index]
+        cell.titleCard.textColor = verde3
+        cell.textCard.textColor = verde3
+        cell.layer.cornerRadius = 15
+    }
+    
 }
