d2c2c903e44d157b1a42e9cd944027d4d30bec2e
bia.duque <biaduque7@hotmail.com>
2022-01-14 23:11:07 -0300

    Adicionar botão de criacao da devocional em Leituras Rapidas

--- MeuDevocional/DevocionaisDiarias/DevocionalDiarioRapidoViewController.swift
+++ MeuDevocional/DevocionaisDiarias/DevocionalDiarioRapidoViewController.swift
@@ -18,6 +18,35 @@ class DevocionalDiarioRapidoViewController: UIViewController {
         content()
     }
     
+    
+    @IBAction func newDevocionalButton(_ sender: Any) {
+    }
+    
+    // MARK: Prepare (edicao e devocional)
+    ///funcao que ira gerar o modal para a criacao da nova colecction
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        ///adiciona o card que sera apenas editado na proxima view...
+        let _ = try? CoreDataStack.createDevocional(titulo: self.titleLabel?.text ?? "",
+                                                    baseBiblica: "",
+                                                    contextualizacao: "",
+                                                    reflexao: "",
+                                                    conclusao: "",
+                                                    aplicacao1: "",
+                                                    aplicacao2: "",
+                                                    aplicacao3: "",
+                                                    backgroundColor: "1",
+                                                    backgroundImage: "crie2",
+                                                    link: "",livro: "",
+                                                    capitulo: "",
+                                                    versiculo: "",
+                                                    data: "")
+        
+        let vc = segue.destination as! MinhaDevocional3ViewController
+        let index = try? CoreDataStack.getDevocional().count
+        vc.edit = true
+        vc.indice = (index ?? 1) - 1
+   }
+    
     func content(){
         self.titleLabel?.text = devocional?.titulo
         self.refBiblicaLabel?.text = devocional?.refBiblica
