cb0094c13fc91747ef3a47fd7654c6276b7306d3
bia.duque <biaduque7@hotmail.com>
2022-01-26 16:35:11 -0300

    Corrigir novamente o fundo das celulas do mural

--- MeuDevocional/ViewControllersCell/MyCollectionViewCell2.swift
+++ MeuDevocional/ViewControllersCell/MyCollectionViewCell2.swift
@@ -15,44 +15,26 @@ class MyCollectionViewCell2: UICollectionViewCell{
     @IBOutlet weak var data: UILabel!
     
     func setImage(image: String,color: String){
-        if image == "postit1"{
-            self.backgroundColor = UIColor(named: "Verde2")
-            self.setDefault()
-        }
-        else if image == "postit2"{
-            self.backgroundColor = UIColor(named: "Amarelo1")
-            self.setDefault()
-        }
-        else if image == "postit3"{
-            self.backgroundColor = UIColor(named: "Amarelo2")
-            self.setDefault()
-        }
-        else if image == "postit4"{
-            self.backgroundColor = UIColor(named: "Amarelo3")
-            self.setDefault()
-        }
-        else if image == "novopost" || image == "base1" || image == "base2"{
-            self.background.image = UIImage(named: image)
-            self.backgroundColor = .clear
-            self.background.alpha = 1
-        }
-        else{
-            let path = image
-            let imageNew = UIImage(contentsOfFile: SalvarImagem.getFilePath(fileName: path))
-            self.background.image = imageNew
-            self.background.alpha = 0.6
-            self.background.layer.cornerRadius = 10
-            self.background.contentMode = .scaleAspectFill
-            self.nota.backgroundColor = UIColor(named: color)
-            self.data.backgroundColor = UIColor(named: color)
-            self.backgroundColor = .clear
-        }
-    }
-    
-    func setDefault(){
-        self.background.image = nil
-        self.background.alpha = 1
-        self.nota.backgroundColor = .clear
-        self.data.backgroundColor = .clear
+           if image != "postit1" &&
+               image != "postit2" &&
+               image != "postit3" &&
+               image != "postit4" &&
+               image != "novopost" {
+               
+               let path = image
+               let imageNew = UIImage(contentsOfFile: SalvarImagem.getFilePath(fileName: path))
+               self.background.image = imageNew
+               self.background.alpha = 0.6
+               self.background.layer.cornerRadius = 10
+               self.background.contentMode = .scaleAspectFill
+               self.nota.backgroundColor = UIColor(named: color)
+               self.data.backgroundColor = UIColor(named: color)
+           }
+           else{
+               self.background.image = UIImage(named: image)
+               self.background.alpha = 1
+               self.nota.backgroundColor = .clear
+               self.data.backgroundColor = .clear
+           }
     }
 }
